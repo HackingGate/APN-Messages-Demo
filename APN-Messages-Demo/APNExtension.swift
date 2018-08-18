6c1421c01f092085c91abcc2c5a4a84d76256e69
Hacking Gate <hackinggate@icloud.com>
2018-08-18 17:21:43 +0900

    Initial commit

---
+++ APN-Messages-Demo/APNExtension.swift
@@ -0,0 +1,109 @@
+//
+//  APNExtension.swift
+//  APN-Messages-Demo
+//
+//  Created by ERU on 2018/08/18.
+//  Copyright © 2018 HackingGate. All rights reserved.
+//
+
+import UIKit
+import CloudKit
+import UserNotifications
+
+let GlobalNotificationType = "GlobalNotification"
+
+extension UIApplication {
+    func registerAPN(_ appDelegate: AppDelegate, launchOptions: [UIApplication.LaunchOptionsKey: Any]?) {
+        UNUserNotificationCenter.current().delegate = appDelegate
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound], completionHandler: { authorized, error in
+            if authorized {
+                DispatchQueue.main.async {
+                    self.registerForRemoteNotifications()
+                }
+            }
+        })
+        
+        if let options: NSDictionary = launchOptions as NSDictionary? {
+            let remoteNotification =
+                options[UIApplication.LaunchOptionsKey.remoteNotification]
+            
+            if let notification = remoteNotification {
+                appDelegate.application(self, didReceiveRemoteNotification:
+                    notification as! [AnyHashable : Any],
+                                 fetchCompletionHandler:  { (result) in
+                })
+                
+            }
+        }
+    }
+
+}
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+        
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        
+        let token = deviceToken.map { String(format: "%02.2hhx", $0) }.joined()
+        print("deviceToken: \(token)")
+        
+        let subscription = CKQuerySubscription(recordType: GlobalNotificationType, predicate: NSPredicate(format: "TRUEPREDICATE"), options: .firesOnRecordCreation)
+
+        
+        let info = CKSubscription.NotificationInfo()
+//        info.alertBody = "alert body"
+//        info.shouldBadge = true
+        info.soundName = "default"
+        
+        // How to show data from a CKRecord in alertBody of a Remote Notification?
+        // https://stackoverflow.com/q/46991967/4063462
+        info.alertLocalizationKey = "%1$@"
+        info.alertLocalizationArgs = ["messageBlock"]
+
+        subscription.notificationInfo = info
+
+        CKContainer.default().publicCloudDatabase.save(subscription, completionHandler: { subscription, error in
+            if error == nil {
+                // Subscription saved successfully
+            } else {
+                // An error occurred
+            }
+        })
+        
+    }
+    
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        
+        let viewController: ViewController =
+            self.window?.rootViewController as! ViewController
+        
+        let notification: CKNotification =
+            CKNotification(fromRemoteNotificationDictionary:
+                userInfo as! [String : NSObject])
+        
+        if (notification.notificationType ==
+            CKNotification.NotificationType.query) {
+            
+            let queryNotification =
+                notification as! CKQueryNotification
+            
+            let recordID = queryNotification.recordID
+            
+            viewController.fetchRecord(recordID!)
+        }
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.alert, .sound])
+    }
+    
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        
+        let alert = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) {
+            UIAlertAction in
+            NSLog("OK Pressed")
+        }
+        alert.addAction(okAction)
+        self.window?.rootViewController?.present(alert, animated: true, completion: nil)
+    }
+}
---
+++ APN-Messages-Demo/ViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  APN-Messages-Demo
+//
+//  Created by ERU on 2018/08/18.
+//  Copyright © 2018 HackingGate. All rights reserved.
+//
+
+import UIKit
+import CloudKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var textField: UITextField!
+    
+    let publicCloudDatabase = CKContainer.default().publicCloudDatabase
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func fetchRecord(_ recordID: CKRecord.ID) {
+        self.publicCloudDatabase.fetch(withRecordID: recordID) { (record: CKRecord?, error: Error?) in
+            if let error = error {
+                print("CKRecord: \(String(describing: record)) fetch failed: \(error)")
+            }
+            if let record = record {
+                print(record)
+                
+                guard let messageBlock = record.object(forKey: "messageBlock") as? String else { return }
+                let alert = UIAlertController(title: "Message", message: messageBlock, preferredStyle: .alert)
+                let okAction = UIAlertAction(title: "OK", style: .default) {
+                    UIAlertAction in
+                    NSLog("OK Pressed")
+                }
+                alert.addAction(okAction)
+                self.present(alert, animated: true, completion: nil)
+            }
+        }
+    }
+
+    @IBAction func sendAction(_ sender: UIButton) {
+        let newGlobalNotificationRecord = CKRecord(recordType: GlobalNotificationType)
+        newGlobalNotificationRecord["messageBlock"] = textField.text
+        self.publicCloudDatabase.save(newGlobalNotificationRecord, completionHandler: { (record: CKRecord?, error: Error?) in
+            if let error = error {
+                print("CKRecord: \(String(describing: record)) save failed: \(error)")
+            }
+        })
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
